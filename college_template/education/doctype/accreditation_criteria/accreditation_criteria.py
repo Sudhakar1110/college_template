cbfb25e10e958727e152e636ead862fb4ffe42d4
Sujai <sujaiit0696@gmail.com>
2026-07-29 11:35:34 +0530

    Fix: Create Python/JS module files for child table DocTypes to resolve migration ModuleNotFoundError

---
+++ college_template/education/doctype/accreditation_criteria/accreditation_criteria.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2026, Bizaxl and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+
+# Child table DocType - no standalone controller needed
+pass
---
+++ college_template/education/doctype/revaluation_application_item/revaluation_application_item.js
@@ -0,0 +1,4 @@
+// Copyright (c) 2026, Bizaxl and contributors
+// For license information, please see license.txt
+
+// Child table DocType - no standalone UI needed
